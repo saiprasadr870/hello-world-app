7505cba77b0af8e28360b58a3ffe5ebfc7348f47
saiprasadr870 <105206375+saiprasadr870@users.noreply.github.com>
2024-09-26 18:37:17 +0530

    Create app.js
    added new files

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const http = require('http');
+
+const hostname = '3.82.122.149';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
